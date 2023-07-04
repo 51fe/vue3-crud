5b382d16b101c0c80240afc552bcd24a98f156cd
51fe <riafan@hotmail.com>
2023-07-04 17:04:24 +0800

    packing optimazition

---
+++ src/Provider.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import './styles/index.scss'
+import "element-plus/theme-chalk/el-message.css";
+import "element-plus/theme-chalk/el-message-box.css";
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import App from './App.vue';
+</script>
+
+<template>
+<el-config-provider :locale="zhCn">
+  <App />
+</el-config-provider>
+</template>
